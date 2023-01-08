25ca0632404096608cd13556487ce9335c0db456
Rawan Bazadough <bazadough1997@gmail.com>
2023-01-08 14:32:03 +0300

    palceholder fixed

--- src/forms/SearchForm.js
+++ src/forms/SearchForm.js
@@ -4,7 +4,7 @@ export const SearchForm = ({ changeHandler }) => {
   return (
     <div style={{ margin: "10px" }}>
       <Form.Label>Search by country:</Form.Label>
-      <Form.Control onChange={changeHandler} type="text" />
+      <Form.Control onChange={changeHandler} type="text" placeholder="enter a country name.." />
     </div>
   );
 };
