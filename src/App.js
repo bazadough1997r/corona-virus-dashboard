26264da0baa412f887b18a4f9172ce0a7d6a9fd2
Rawan Bazadough <bazadough1997@gmail.com>
2023-01-08 15:58:10 +0300

    scroll to top btn added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import CountryCard from "./layouts/CountryCard";
 import Header from "./layouts/Header";
 import HomeCards from "./layouts/HomeCards";
 import Map from "./layouts/Map";
+import ScrollToTop from "./layouts/ScrollToTop";
 
 const App = () => {
   return (
     <>
       <Header />
       <HomeCards />
-      <Map/>
+      <Map />
       <CountryCard />
+      <ScrollToTop />
     </>
   );
 };
