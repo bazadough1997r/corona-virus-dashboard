f633ca1a1643da4fe763ea2a8c776b94fbf96328
Rawan Bazadough <bazadough1997@gmail.com>
2023-01-08 14:28:47 +0300

    buttons styled

--- src/layouts/RankCountries.js
+++ src/layouts/RankCountries.js
@@ -1,16 +1,32 @@
 import Button from "react-bootstrap/Button";
-import InputGroup from "react-bootstrap/InputGroup";
+import { Card, CardGroup } from "react-bootstrap";
 
 export const RankCountries = ({ highestBtnHandler, lowestBtnHandler }) => {
   return (
-    <InputGroup className="mb-3" style={{ margin: "10px", width: "99%" }}>
-      <Button onClick={lowestBtnHandler} variant="outline-success">
-        Lowest to highest
-      </Button>
-      <Button onClick={highestBtnHandler} variant="outline-danger">
-        Highest to lowest
-      </Button>
-    </InputGroup>
+    <Card
+      bg="dark"
+      key="dark"
+      text="white"
+      className="text-center"
+      style={{ margin: "10px" }}
+    >
+      <CardGroup style={{ margin: "10px" }}>
+        <Button
+          style={{ margin: "0px 10px" }}
+          onClick={lowestBtnHandler}
+          variant="success"
+        >
+          Lowest to highest
+        </Button>
+        <Button
+          style={{ margin: "0px 10px" }}
+          onClick={highestBtnHandler}
+          variant="danger"
+        >
+          Highest to lowest
+        </Button>
+      </CardGroup>
+    </Card>
   );
 };
 
